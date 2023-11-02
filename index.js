94e23a665aa611773c09b4eae442b64f136af6bf
yuanfajun0912 <yuanfajun0912@126.com>
2023-11-02 20:36:27 +0800

    feat: add command-line-args&&command-line-usage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { dirname, resolve } from 'path'
 import { readFile, writeFileSync, existsSync, mkdirSync } from 'fs'
 import { createRandomPicker } from './lib/random.js'
 import { generate } from './lib/generator.js'
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -35,22 +37,62 @@ const saveCorpus = (title, data) => {
   }
   // 写入文件
   writeFileSync(outputFile, data)
+  return outputFile
 }
 
 const parseOptions = () => {
-  let options = {}
-  const argv = process.argv
-  argv.forEach((item, index) => {
-    if (item === '--title' && argv[index + 1]) {
-      options.title = argv[index + 1]
-    } else if (item === '--min' && argv[index + 1]) {
-      options.min = argv[index + 1]
-    } else if (item === '--max' && argv[index + 1]) {
-      options.max = argv[index + 1]
-    } 
-  })
-  return options
+  // let options = {}
+  // const argv = process.argv
+  // argv.forEach((item, index) => {
+  //   if (item === '--title' && argv[index + 1]) {
+  //     options.title = argv[index + 1]
+  //   } else if (item === '--min' && argv[index + 1]) {
+  //     options.min = argv[index + 1]
+  //   } else if (item === '--max' && argv[index + 1]) {
+  //     options.max = argv[index + 1]
+  //   } 
+  // })
+  // return options
+  const optionDefinitions = [
+    {name: 'title', alias: 't', type: String},
+    {name: 'min', type: Number},
+    {name: 'max', type: Number},
+    {name: 'help'}
+  ]
+  return commandLineArgs(optionDefinitions)
 }
+
+const usageSections = [
+  {
+    header: '狗屁不通文章生成器--by yfj',
+    content: '生成随机的文章段落用于测试',
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'title',
+        typeLabel: '{underline string}',
+        description: '文章的主题。'
+      },
+      {
+        name: 'min',
+        typeLabel: '{underline number}',
+        description: '文章最小字数。'
+      },
+      {
+        name: 'max',
+        typeLabel: '{underline number}',
+        description: '文章最大字数。'
+      },
+      {
+        name: 'help',
+        typeLabel: '{underline string}',
+        description: '命令行参数手册'
+      }
+    ],
+  },
+]
 
 // 加载词法库
 loadCorpus().then(({ status, data }) => {
@@ -61,12 +103,18 @@ loadCorpus().then(({ status, data }) => {
   const defaultTitle = randomTitleFunc()
   // 获取命令行的参数
   const argvOptions = parseOptions()
-  // 文章title
-  const title = argvOptions.title || defaultTitle
-  // 生成的文章列表
-  const article = generate(`${title}`, { corpus: data, min: argvOptions.min || 1000, max: argvOptions.max || 2000 })
-  // 格式化文章
-  const articleStr = `${title}\n\n    ${article.join('\n    ')}`
-  // 保存文章
-  saveCorpus(`${title}-${new Date().getTime()}`, articleStr)
+  // 命令行输入的--help就打印参数手册
+  if ('help' in argvOptions) {
+    console.log(commandLineUsage(usageSections))
+  } else {
+    // 文章title
+    const title = argvOptions.title || defaultTitle
+    // 生成的文章列表
+    const article = generate(`${title}`, { corpus: data, min: argvOptions.min || 1000, max: argvOptions.max || 2000 })
+    // 格式化文章
+    const articleStr = `${title}\n\n    ${article.join('\n    ')}`
+    // 保存文章
+    const saveFileUrl = saveCorpus(`${title}-${new Date().getTime()}`, articleStr)
+    console.log(`保存文章成功，目录在 ${saveFileUrl}`)
+  }
 })
