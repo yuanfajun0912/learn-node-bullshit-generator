6d8e1896f9a0e9813bebbcf053dfea157f2a94b6
yuanfajun0912 <yuanfajun0912@126.com>
2023-11-02 20:48:56 +0800

    feat: 模块化组织项目结构

--- index.js
+++ index.js
@@ -1,98 +1,7 @@
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-import { readFile, writeFileSync, existsSync, mkdirSync } from 'fs'
 import { createRandomPicker } from './lib/random.js'
 import { generate } from './lib/generator.js'
-import commandLineArgs from 'command-line-args'
-import commandLineUsage from 'command-line-usage';
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-const loadCorpus = async () => {
-  return new Promise((resolveFunc, rejectFunc) => {
-    readFile(resolve(__dirname, './corpus/data.json'), 'utf-8', (err, data) => {
-      if (err) {
-        resolveFunc({
-          status: false,
-          data: err
-        })
-      } else {
-        resolveFunc({
-          status: true,
-          data: JSON.parse(data)
-        })
-      }
-    })
-  })
-}
-
-const saveCorpus = (title, data) => {
-  // 输出目录path
-  const outputDir = resolve(__dirname, './output')
-  // 输出文件path
-  const outputFile = resolve(outputDir, `${title}.txt`)
-  // 输出目录不存在就创建个
-  if (!existsSync(outputDir)) {
-    mkdirSync(outputDir)
-  }
-  // 写入文件
-  writeFileSync(outputFile, data)
-  return outputFile
-}
-
-const parseOptions = () => {
-  // let options = {}
-  // const argv = process.argv
-  // argv.forEach((item, index) => {
-  //   if (item === '--title' && argv[index + 1]) {
-  //     options.title = argv[index + 1]
-  //   } else if (item === '--min' && argv[index + 1]) {
-  //     options.min = argv[index + 1]
-  //   } else if (item === '--max' && argv[index + 1]) {
-  //     options.max = argv[index + 1]
-  //   } 
-  // })
-  // return options
-  const optionDefinitions = [
-    {name: 'title', alias: 't', type: String},
-    {name: 'min', type: Number},
-    {name: 'max', type: Number},
-    {name: 'help'}
-  ]
-  return commandLineArgs(optionDefinitions)
-}
-
-const usageSections = [
-  {
-    header: '狗屁不通文章生成器--by yfj',
-    content: '生成随机的文章段落用于测试',
-  },
-  {
-    header: 'Options',
-    optionList: [
-      {
-        name: 'title',
-        typeLabel: '{underline string}',
-        description: '文章的主题。'
-      },
-      {
-        name: 'min',
-        typeLabel: '{underline number}',
-        description: '文章最小字数。'
-      },
-      {
-        name: 'max',
-        typeLabel: '{underline number}',
-        description: '文章最大字数。'
-      },
-      {
-        name: 'help',
-        typeLabel: '{underline string}',
-        description: '命令行参数手册'
-      }
-    ],
-  },
-]
+import { loadCorpus, saveCorpus } from './lib/corpus.js'
+import { parseOptions, getCommonGuide } from './lib/option.js'
 
 // 加载词法库
 loadCorpus().then(({ status, data }) => {
@@ -105,7 +14,7 @@ loadCorpus().then(({ status, data }) => {
   const argvOptions = parseOptions()
   // 命令行输入的--help就打印参数手册
   if ('help' in argvOptions) {
-    console.log(commandLineUsage(usageSections))
+    console.log(getCommonGuide())
   } else {
     // 文章title
     const title = argvOptions.title || defaultTitle
