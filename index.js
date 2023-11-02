5abb93b368539b933abac54474986c2ce89abb87
yuanfajun0912 <yuanfajun0912@126.com>
2023-11-02 20:02:07 +0800

    feat: 通过process.argv获取命令行参数

--- index.js
+++ index.js
@@ -37,17 +37,36 @@ const saveCorpus = (title, data) => {
   writeFileSync(outputFile, data)
 }
 
+const parseOptions = () => {
+  let options = {}
+  const argv = process.argv
+  argv.forEach((item, index) => {
+    if (item === '--title' && argv[index + 1]) {
+      options.title = argv[index + 1]
+    } else if (item === '--min' && argv[index + 1]) {
+      options.min = argv[index + 1]
+    } else if (item === '--max' && argv[index + 1]) {
+      options.max = argv[index + 1]
+    } 
+  })
+  return options
+}
+
 // 加载词法库
 loadCorpus().then(({ status, data }) => {
   if (!status) return console.log('err:', data) 
   // 随机获取title方法
   const randomTitleFunc = createRandomPicker(data.title)
   // 获取到的随机title
-  const title = randomTitleFunc()
+  const defaultTitle = randomTitleFunc()
+  // 获取命令行的参数
+  const argvOptions = parseOptions()
+  // 文章title
+  const title = argvOptions.title || defaultTitle
   // 生成的文章列表
-  const article = generate(title, { corpus: data })
+  const article = generate(`${title}`, { corpus: data, min: argvOptions.min || 1000, max: argvOptions.max || 2000 })
   // 格式化文章
   const articleStr = `${title}\n\n    ${article.join('\n    ')}`
   // 保存文章
-  saveCorpus('废话文章1', articleStr)
+  saveCorpus(`${title}-${new Date().getTime()}`, articleStr)
 })
